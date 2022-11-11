e2c1b9f53ce92acce3655b61cd5a9072ddafe0ab
Vít Radosta <radosta@enigoo.cz>
2022-11-11 12:53:25 +0100

    css update

--- src/App.js
+++ src/App.js
@@ -41,15 +41,15 @@ const App = () => {
     }, [])
     return (
         <div className="max-w-[1440px] h-screen w-screen m-auto bg-[#1f2632] flex flex-col justify-center items-center">
-            <div className={mobile ? "w-[90%] m-auto bg-[#323a49] h-[320px] rounded-2xl flex flex-col  items-center relative" : "w-2/6 h-2/6 bg-[#323a49] m-auto rounded-2xl flex flex-col  items-center relative"}>
+            <div className={mobile ? "w-[90%] m-auto bg-[#323a49] h-[320px] rounded-2xl flex flex-col  items-center relative" : "w-2/6  h-[34%] bg-[#323a49]  rounded-2xl flex flex-col  items-center relative"}>
                 <h2 className="text-[#52ffa8] pt-10 pb-6 md:pb-8 text-xs  font-Manrope tracking-[3px]  uppercase">Advice # {advice?.slip?.id} </h2>
-                <p className="text-[#cee3e9] text-2xl min-h-[40%] font-Manrope max-h-[40%] max-w-[80%] text-center">"{advice?.slip?.advice}"</p>
+                <p className="text-[#cee3e9] text-2xl min-h-[40%] font-Manrope max-h-[40%] max-w-[80%] text-center">“{advice?.slip?.advice}“</p>
                 <img src={mobile ? deviderM : deviderD} className="py-10 md:py-5 max-w-[80%] dragable" alt="devider" draggable="false" />
                 <div className="bg-[#52ffa8] absolute h-14 w-14 rounded-full -bottom-7 m-auto flex justify-center items-center cursor-pointer shadow-[0_px0_px_0px_30px]  hover:shadow-neon-green" onClick={() => apiGetAdvice()} >
                     <img src={dice} alt="dice" />
                 </div>
             </div>
-            <div className={mobile ? "attribution mt-5" : "attribution"}>
+            <div className={mobile ? "attribution absolute bottom-0 " : " absolute bottom-0 attribution 0"}>
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>.
                 Coded by <a href="#">Vít Radosta</a>.
             </div>
